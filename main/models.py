4cb40bc44d90220272d101091b370763f2dfc3ae
linhngocdang <danglinh752001@gmail.com>
2023-06-19 13:50:22 +0700

    fia

--- main/models.py
+++ main/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from keras.preprocessing.image import load_img, img_to_array
 import numpy as np
 import tensorflow as tf
 import os
+import cv2
 
 class Employee(models.Model):
     full_name = models.CharField(max_length=200)
@@ -17,8 +17,24 @@ class Employee(models.Model):
     classified = models.CharField(max_length=200, blank=True)
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
 
-    # def __str__(self):
-    #     return self.full_name
+    @staticmethod
+    def crop_image_from_gray(img, tol=7):
+        if img.ndim == 2:
+            mask = img > tol
+            return img[np.ix_(mask.any(1), mask.any(0))]
+        elif img.ndim == 3:
+            gray_img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+            mask = gray_img > tol
+            
+            check_shape = img[:, :, 0][np.ix_(mask.any(1), mask.any(0))].shape[0]
+            if check_shape == 0: 
+                return img 
+            else:
+                img1 = img[:, :, 0][np.ix_(mask.any(1), mask.any(0))]
+                img2 = img[:, :, 1][np.ix_(mask.any(1), mask.any(0))]
+                img3 = img[:, :, 2][np.ix_(mask.any(1), mask.any(0))]
+                img = np.stack([img1, img2, img3], axis=-1)
+            return img
 
     def save(self, *args, **kwargs):
         try:
@@ -31,8 +47,8 @@ class Employee(models.Model):
             else:
                 print("Model file does not exist.")
             img_path = self.featured_img.path
-            img = load_img(img_path, target_size=(512, 512))
-            img_array = img_to_array(img)
+            img = load_img(img_path)
+            img_array = self.crop_image_from_gray(img, tol=7)
             img_array = img_array / 255.0 
             img_array = np.expand_dims(img_array, axis=0)
             model = tf.keras.models.load_model('hrd_model.h5')
@@ -45,12 +61,9 @@ class Employee(models.Model):
         except Exception as e:
             print('Classification failed:', e)
         super().save(*args, **kwargs)
-        
+
 class Doctor(models.Model):
     full_name = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
     password = models.CharField(max_length=100)
-
-    # def __str__(self):
-    #     return self.full_name
     
