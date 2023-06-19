e3157553af81a80c0c45c1de1372f8d742caa621
linhngocdang <danglinh752001@gmail.com>
2023-06-19 18:15:46 +0700

    hehe

--- main/models.py
+++ main/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from keras.preprocessing.image import load_img, img_to_array
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
 import numpy as np
 import tensorflow as tf
 import os
@@ -50,9 +50,9 @@ class Employee(models.Model):
             #img = load_img(img_path)
             # img_array = self.crop_image_from_gray(img, tol=7)
             img = load_img(img_path, target_size=(512, 512))
-            #img_array = img_to_array(img)
-            img_array = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            img_array = img_to_array(img)
             img_array = img_array / 255.0 
+            #img_array = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             img_array = np.expand_dims(img_array, axis=0)
             model = tf.keras.models.load_model('hrd_model.h5')
             prediction = model.predict(img_array)
