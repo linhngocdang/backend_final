9676ef550c39010d16fe1fb54f09120031a5a643
linhngocdang <danglinh752001@gmail.com>
2023-06-19 11:56:25 +0700

    finall

--- djangoreactproject/urls.py
+++ djangoreactproject/urls.py
@@ -21,12 +21,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('', include('main.urls')),
-# ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-]+staticfiles_urlpatterns(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', include('main.urls')),
+# ]+staticfiles_urlpatterns(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
