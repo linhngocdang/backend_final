312f552f36f485e18608940ac793a9d92977d985
linhngocdang <danglinh752001@gmail.com>
2023-06-22 15:41:38 +0700

    commit json

--- main/models.py
+++ main/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from keras.preprocessing.image import load_img, img_to_array
+from keras.models import model_from_json
 import numpy as np
 import tensorflow as tf
 import os
-import cv2
 
 class Employee(models.Model):
     full_name = models.CharField(max_length=200)
@@ -15,26 +15,46 @@ class Employee(models.Model):
     birthday = models.CharField(max_length=20, null=True, blank=True)
     featured_img = models.ImageField()
     classified = models.CharField(max_length=200, blank=True)
-    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+
+    def __str__(self):
+        return self.full_name
 
     def save(self, *args, **kwargs):
         try:
-            model_file = 'hrd_model.h5'
+            model_file = 'modelResnet.json'
+            weights_file = 'modelResnet.h5'
             model_path = os.path.join(os.getcwd(), model_file)
+            weights_path = os.path.join(os.getcwd(), weights_file)
             print('Saving employee:', self.full_name)
             print('Image path:', self.featured_img.path)
-            if os.path.isfile(model_path):
-                print("Model file exists.")
+            
+            if os.path.isfile(model_path) and os.path.isfile(weights_path):
+                print("Model files exist.")
             else:
-                print("Model file does not exist.")
+                print("Model files do not exist.")
+                return
+
             img_path = self.featured_img.path
             img = load_img(img_path, target_size=(512, 512))
-            #img_array = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             img_array = img_to_array(img)
             img_array = img_array / 255.0 
             img_array = np.expand_dims(img_array, axis=0)
-            model = tf.keras.models.load_model('hrd_model.h5')
-            prediction = model.predict(img_array)
+
+            with open(model_file, "r") as json_file:
+                loaded_model_json = json_file.read()
+            loaded_model = model_from_json(loaded_model_json)
+
+
+            loaded_model.load_weights(weights_file)
+
+            weights = loaded_model.get_weights()
+            
+            print("Weights of the first layer:")
+            print(weights[0])
+
+            loaded_model.set_weights(weights)
+
+            prediction = loaded_model.predict(img_array)
             class_id = np.argmax(prediction)
             class_names = ['NO DR', 'MILD', 'MODERATE', 'SEVERE', 'PROLIFERATIVE DR']
             class_name = class_names[class_id]
@@ -43,7 +63,7 @@ class Employee(models.Model):
         except Exception as e:
             print('Classification failed:', e)
         super().save(*args, **kwargs)
-
+        
 class Doctor(models.Model):
     full_name = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
