a0e8accb448983d01d8cc3a49fa96f2e4fa850d6
bhuyenef <mylee736@gmail.com>
2023-06-25 08:29:12 +0700

    change json

--- djangoreactproject/settings.py
+++ djangoreactproject/settings.py
@@ -89,8 +89,6 @@ WSGI_APPLICATION = 'djangoreactproject.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
--- main/models.py
+++ main/models.py
@@ -4,6 +4,7 @@ from keras.models import model_from_json
 import numpy as np
 import tensorflow as tf
 import os
+import json
 
 class Employee(models.Model):
     full_name = models.CharField(max_length=200)
@@ -15,25 +16,21 @@ class Employee(models.Model):
     birthday = models.CharField(max_length=20, null=True, blank=True)
     featured_img = models.ImageField()
     classified = models.CharField(max_length=200, blank=True)
-    #updated = models.DateTimeField(auto_now=True, auto_now_add=False)
-    
+    # updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+
     def __str__(self):
         return self.full_name
 
     def save(self, *args, **kwargs):
         try:
-            model_file = 'modelResnet.json'
-            weights_file = 'modelResnet.hdf5'
-            model_path = os.path.join(os.getcwd(), model_file)
-            weights_path = os.path.join(os.getcwd(), weights_file)
-            print('Saving employee:', self.full_name)
-            print('Image path:', self.featured_img.path)
-            
-            if os.path.isfile(model_path) and os.path.isfile(weights_path):
-                print("Model files exist.")
-            else:
-                print("Model files do not exist.")
-                return
+            json_file = 'modelResnet.json'
+            with open(json_file, 'r') as file:
+                model_json = file.read()
+
+
+            model = model_from_json(model_json)
+
+            model.load_weights(json_file)
 
             img_path = self.featured_img.path
             img = load_img(img_path, target_size=(512, 512))
@@ -41,21 +38,7 @@ class Employee(models.Model):
             img_array = img_array / 255.0 
             img_array = np.expand_dims(img_array, axis=0)
 
-            with open(model_file, "r") as json_file:
-                loaded_model_json = json_file.read()
-            loaded_model = model_from_json(loaded_model_json)
-
-
-            loaded_model.load_weights(weights_file)
-
-            weights = loaded_model.get_weights()
-            
-            print("Weights of the first layer:")
-            print(weights[0])
-
-            loaded_model.set_weights(weights)
-
-            prediction = loaded_model.predict(img_array)
+            prediction = model.predict(img_array)
             class_id = np.argmax(prediction)
             class_names = ['NO DR', 'MILD', 'MODERATE', 'SEVERE', 'PROLIFERATIVE DR']
             class_name = class_names[class_id]
@@ -64,7 +47,7 @@ class Employee(models.Model):
         except Exception as e:
             print('Classification failed:', e)
         super().save(*args, **kwargs)
-        
+
 class Doctor(models.Model):
     full_name = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
